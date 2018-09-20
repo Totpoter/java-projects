6347c32b80b529cb0c9a69d7ddb2628c77bcca0c
Totpoter <mindroixweetosser@gmail.com>
2018-09-20 09:19:09 +0200

    Gave appropriate error messages and commented out some more

--- scanner_intro/src/scanner_intro/scanner_intro.java
+++ scanner_intro/src/scanner_intro/scanner_intro.java
@@ -19,7 +19,7 @@ public class scanner_intro {
 		/* Klass input */
 		System.out.println("Vilken klass går du i?");
 		String klass=input.nextLine();
-		/* Printa ut allt och stänga scannern */
+		/* Kolla så att alla fält är ifyllda */
 		boolean works=true;
 		 if(name.isEmpty()) { 
 			 System.out.println("Du måste skriva in ditt namn." );
@@ -30,11 +30,13 @@ public class scanner_intro {
 		 if(telenmr.isEmpty()) {
 			 System.out.println("Du måste skriva in ditt telefonnummer."); 
 			 works=false;}
+		 /* Kolla så att telefonnummer har 10 siffror i sig*/
 		 boolean numberWorks = true;
 		 if(telenmr.length() != 10) {
-			 System.out.println("WoooooooooooooooooooW");
+			 System.out.println("Ditt telefonnummer måste ha 10 siffror.");
 			 works = false;
 		 }
+		 /* Kolla så att telefonnummret endast innehåller siffror */
 		 for(int i = 0; i < telenmr.length(); i++)
 		 {
 			 if(!Character.isDigit(telenmr.charAt(i)))
@@ -44,13 +46,14 @@ public class scanner_intro {
 		 }
 		 
 		 if(!numberWorks) {
-			 System.out.println("WooooooW");
+			 System.out.println("Ditt telefonnummer kan endast ha siffror i sig.");
 			 works = false;
 		 }
 		 
 		 if(klass.isEmpty()) {
 			 System.out.println("Du måste skriva in din klass."); 
 			 works=false;}
+		 /* Ifall allt funkar borde detta skrivas ut */
 		 if(works == true) { System.out.println("ditt namn är " + name + ", och du bor på " + adress + ". Ditt telefonnummer är " + telenmr + " och du går i " + klass);
 			input.close(); }
 		}
