4d6cd379daa9d99e310b385aed9e4286d8359fca
Totpoter <mindroixweetosser@gmail.com>
2018-10-19 15:19:30 +0200

    finished

--- v41/src/v41/sjörövarspråk.java
+++ v41/src/v41/sjörövarspråk.java
@@ -1,9 +1,26 @@
 package v41;
 public class sjörövarspråk {
 	public static void main(String[] args) {
-		sjorovare("Hej");
+		System.out.println(sjorovare("mening på sjörövarspråket"));
+		
 	}
 	public static String sjorovare(String str) {
 		char match[] = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'z', 'x'};
+		String res = "";
+		for(int i = 0; i < str.length(); i++) {
+			boolean found = false;
+			for(int j = 0; j < match.length; j++) {
+				if(str.charAt(i) == match[j]) {
+					found = true;
+					break;
+				}
+			}
+			if(found) {
+				res += str.charAt(i) + "o" + str.charAt(i);
+			} else {
+				res += str.charAt(i);
+			}
+		}
+		return res;
 	}
 }
